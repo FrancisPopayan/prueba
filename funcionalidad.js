a02c3e637eb03e5c1a6fb8a6bb45bd9fed94132f
Fazt <contact@faztweb.com>
2021-01-08 22:31:02 +0100

    Version ultima

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,1 +1,7 @@
 console.log("Estas son las funcionalidades");
+console.log("Esta es una segunda linea de prueba");
+console.log("Esta es una segunda linea de prueba");
+console.log("Esta es una segunda linea de prueba");
+console.log("Esta es una segunda linea de prueba");
+console.log("Esta es una segunda linea de prueba");
+console.log("Esta es una segunda linea de prueba");
